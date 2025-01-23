08f0b3f873e4c80f64014e101d791a1c60d32a25
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-01-23 17:47:06 +0900

    Add About and Footer components

---
+++ src/app/components/about.tsx
@@ -0,0 +1,43 @@
+import { Card, CardBody } from '@heroui/react'
+
+export default () => {
+  return (
+    <Card className='mx-auto w-full max-w-3xl'>
+      <CardBody className='p-5 lg:p-10'>
+        <h2 className='text-xl md:text-3xl font-bold mb-8 text-center'>
+          カードタイプのリンクジェネレーター
+        </h2>
+        <p className='mt-4 leading-relaxed'>
+          入力したURLのOGP情報をもとにカードが作成されます。
+          <br />
+          URLによっては、正しくカードが作成されません。ご了承ください。
+        </p>
+        <p className='mt-4 leading-relaxed'>
+          生成されたリンクは「デザイン」✕「テーマカラー」✕「ボーダーの有無」を組み合わせることができます。
+        </p>
+        <p className='mt-4 leading-relaxed'>
+          コードは「code」タブをクリックして、
+          <svg
+            aria-hidden='true'
+            fill='none'
+            focusable='false'
+            height='1em'
+            role='presentation'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='1.5'
+            viewBox='0 0 24 24'
+            width='1em'
+            className='text-inherit opacity-100 scale-100 inline-block mx-1'
+          >
+            <path d='M16 17.1c0 3.5-1.4 4.9-4.9 4.9H6.9C3.4 22 2 20.6 2 17.1v-4.2C2 9.4 3.4 8 6.9 8h4.2c3.5 0 4.9 1.4 4.9 4.9Z' />
+            <path d='M8 8V6.9C8 3.4 9.4 2 12.9 2h4.2C20.6 2 22 3.4 22 6.9v4.2c0 3.5-1.4 4.9-4.9 4.9H16' />
+            <path d='M16 12.9C16 9.4 14.6 8 11.1 8' />
+          </svg>
+          をクリックしてコピーできます。
+        </p>
+      </CardBody>
+    </Card>
+  )
+}
