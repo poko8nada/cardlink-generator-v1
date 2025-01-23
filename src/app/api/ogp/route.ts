f5d04809711a351710897356f5408fe308dafff2
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-01-23 17:47:39 +0900

    Refactor OGP process

--- src/app/api/ogp/route.ts
+++ src/app/api/ogp/route.ts
@@ -10,23 +10,6 @@ export async function POST(req: NextRequest) {
 
   console.log(ogp)
 
-  if (ogp.ogUrl && ogp.ogImage && /^https:\/\/www\.amazon*/.test(ogp.ogUrl)) {
-    ogp.ogImage = ogp.ogImage.toReversed().filter(image => {
-      return (
-        image.url.includes('/I/') &&
-        image.url.includes('_SX') &&
-        image.url.includes('_SY')
-      )
-    })
-    ogp.favicon = 'https://www.amazon.co.jp/favicon.ico'
-  }
-  if (ogp.requestUrl && /^https:\/\/www\.google*/.test(ogp.requestUrl)) {
-    ogp.favicon = 'https://www.google.com/favicon.ico'
-  }
-  if (ogp.ogUrl && ogp.ogImage && /^https:\/\/github\.com*/.test(ogp.ogUrl)) {
-    ogp.ogImage[0].url = `${ogp.ogImage[0].url}?s=200`
-  }
-
   if (ogp.ogImage) {
     ogp.ogImage = ogp.ogImage.filter(image => {
       return (
@@ -36,6 +19,13 @@ export async function POST(req: NextRequest) {
         image.url.includes('.jpeg')
       )
     })
+  }
+  if (ogp.ogImage && ogp.requestUrl) {
+    const imageUrl = ogp.ogImage[0].url
+    const url = /^http.*/.test(imageUrl)
+      ? imageUrl
+      : `${ogp.requestUrl.replace(/(^https?:\/\/[^/]+).*$/, '$1')}/${imageUrl.replace(/^\/?/, '').replace(/\/?$/, '')}`
+    ogp.favicon = url
   }
 
   if (ogp.favicon && ogp.requestUrl) {
@@ -48,6 +38,39 @@ export async function POST(req: NextRequest) {
 
   if (ogp.ogUrl && !ogp.ogSiteName) {
     ogp.ogSiteName = new URL(ogp.ogUrl).hostname
+  }
+
+  if (ogp.ogUrl && ogp.ogImage && /https:\/\/www\.amazon.*/.test(ogp.ogUrl)) {
+    ogp.ogImage = ogp.ogImage.toReversed().filter(image => {
+      return (
+        image.url.includes('/I/') &&
+        image.url.includes('_SX') &&
+        image.url.includes('_SY')
+      )
+    })
+    ogp.favicon = 'https://www.amazon.co.jp/favicon.ico'
+  }
+  if (
+    ogp.ogUrl &&
+    ogp.ogImage &&
+    /https:\/\/www\.amazon.*\/gp\/video.*/.test(ogp.ogUrl)
+  ) {
+    ogp.ogImage = ogp.ogImage.filter(image => {
+      return image.url.includes('/pv-target-images/')
+    })
+  }
+
+  if (
+    ogp.requestUrl &&
+    /https:\/\/(www\.|item\.)rakuten\.co\.jp/.test(ogp.requestUrl)
+  ) {
+    ogp.favicon = 'https://www.rakuten.co.jp/favicon.ico'
+  }
+  if (ogp.requestUrl && /^https:\/\/www\.google*/.test(ogp.requestUrl)) {
+    ogp.favicon = 'https://www.google.com/favicon.ico'
+  }
+  if (ogp.ogUrl && ogp.ogImage && /^https:\/\/github\.com*/.test(ogp.ogUrl)) {
+    ogp.ogImage[0].url = `${ogp.ogImage[0].url}?s=200`
   }
 
   return new Response(JSON.stringify(ogp))
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 'use client'
 import { colorTypes, reducer } from '@/lib/colorReducer'
-import { Spinner } from '@heroui/react'
-import { Card, CardBody } from '@heroui/react'
+import { Card, Snippet, Spinner } from '@heroui/react'
 import type { OgObject } from 'open-graph-scraper/types'
 import { useState } from 'react'
 import { useReducer } from 'react'
 import { Suspense } from 'react'
 import URLForm from './components/URLForm'
-import Header from './components/cards/header'
+import About from './components/about'
 import HorizontalA from './components/cards/horizontalA'
 import HorizontalB from './components/cards/horizontalB'
 import PhotoMain from './components/cards/photoMain'
 import Vertical from './components/cards/vertical'
 import ColorPickier from './components/colorPickier'
+import Footer from './components/footer'
+import Header from './components/header'
+import LinkData from './components/linkData'
 import TabMenu from './components/tabMenu'
 
 export default function Home() {
@@ -35,75 +37,57 @@ export default function Home() {
         </div>
       </Header>
       <main className='w-full flex flex-col justify-center items-center'>
-        <section className='container mx-auto flex flex-col justify-center items-center p-4'>
-          {loading && (
-            <Spinner
-              color='default'
-              label='OGPを検索中です'
-              labelColor='foreground'
-              size='lg'
-              className='my-20'
-            />
-          )}
-          {!loading && (
-            <div className={!ogp ? 'hidden' : 'mx-auto w-full max-w-3xl'}>
-              <TabMenu>
-                <Suspense fallback={<Spinner />}>
-                  <HorizontalA ogp={ogp} color={color} isBorder={isBorder} />
-                </Suspense>
-              </TabMenu>
-              <TabMenu>
-                <HorizontalB ogp={ogp} color={color} isBorder={isBorder} />
-              </TabMenu>
-              <TabMenu>
-                <Vertical ogp={ogp} color={color} isBorder={isBorder} />
-              </TabMenu>
-              <TabMenu>
-                <PhotoMain ogp={ogp} color={color} isBorder={isBorder} />
-              </TabMenu>
-            </div>
-          )}
-        </section>
-        <section className='container mx-auto p-4'>
-          <Card className='mx-auto w-full max-w-3xl'>
-            <CardBody className='p-5 lg:p-10'>
-              <h2 className='text-xl md:text-3xl font-bold mb-8 text-center'>
-                カードタイプのリンクジェネレーター
-              </h2>
-              <p className='mt-4 leading-relaxed'>
-                入力したURLのOGP情報をもとにカードが作成されます。
-                <br />
-                URLによっては、正しくカードが作成されません。ご了承ください。
-              </p>
-              <p className='mt-4 leading-relaxed'>
-                生成されたリンクは「デザイン」✕「テーマカラー」✕「ボーダーの有無」を組み合わせることができます。
-              </p>
-              <p className='mt-4 leading-relaxed'>
-                コードは「code」タブをクリックして、
-                <svg
-                  aria-hidden='true'
-                  fill='none'
-                  focusable='false'
-                  height='1em'
-                  role='presentation'
-                  stroke='currentColor'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='1.5'
-                  viewBox='0 0 24 24'
-                  width='1em'
-                  className='text-inherit opacity-100 scale-100 inline-block mx-1'
-                >
-                  <path d='M16 17.1c0 3.5-1.4 4.9-4.9 4.9H6.9C3.4 22 2 20.6 2 17.1v-4.2C2 9.4 3.4 8 6.9 8h4.2c3.5 0 4.9 1.4 4.9 4.9Z' />
-                  <path d='M8 8V6.9C8 3.4 9.4 2 12.9 2h4.2C20.6 2 22 3.4 22 6.9v4.2c0 3.5-1.4 4.9-4.9 4.9H16' />
-                  <path d='M16 12.9C16 9.4 14.6 8 11.1 8' />
-                </svg>
-                をクリックしてコピーできます。
-              </p>
-            </CardBody>
-          </Card>
+        {loading && (
+          <Spinner
+            color='default'
+            label='OGPを検索中です'
+            labelColor='foreground'
+            size='lg'
+            className='my-36'
+          />
+        )}
+        {!loading && (
+          <>
+            <section
+              className={
+                !ogp
+                  ? 'hidden p-0 m-0'
+                  : 'container mx-auto flex flex-col justify-center items-center p-4'
+              }
+            >
+              <div className='mx-auto w-full max-w-3xl'>
+                <TabMenu>
+                  <Suspense fallback={<Spinner />}>
+                    <HorizontalA ogp={ogp} color={color} isBorder={isBorder} />
+                  </Suspense>
+                </TabMenu>
+                <TabMenu>
+                  <HorizontalB ogp={ogp} color={color} isBorder={isBorder} />
+                </TabMenu>
+                <TabMenu>
+                  <Vertical ogp={ogp} color={color} isBorder={isBorder} />
+                </TabMenu>
+                <TabMenu>
+                  <PhotoMain ogp={ogp} color={color} isBorder={isBorder} />
+                </TabMenu>
+              </div>
+            </section>
+            <section
+              className={
+                !ogp ? 'hidden p-0 m-0' : 'container mx-auto p-4 mt-20'
+              }
+            >
+              <div className={!ogp ? 'hidden' : 'mx-auto w-full max-w-3xl'}>
+                <LinkData ogp={ogp} />
+              </div>
+            </section>
+          </>
+        )}
+        <section className='container mx-auto p-4 mt-20'>
+          <About />
         </section>
       </main>
+      <Footer />
     </>
   )
 }
