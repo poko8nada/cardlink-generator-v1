f00d3d107cbd1a9dc8f8f29c577b1679716028f9
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-01-23 14:06:01 +0900

    Add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { colorTypes, reducer } from '@/lib/colorReducer'
 import { Spinner } from '@heroui/react'
+import { Card, CardBody } from '@heroui/react'
 import type { OgObject } from 'open-graph-scraper/types'
 import { useState } from 'react'
 import { useReducer } from 'react'
@@ -41,7 +42,7 @@ export default function Home() {
             label='OGPを検索中です'
             labelColor='foreground'
             size='lg'
-            className='mt-20'
+            className='my-20'
           />
         )}
         {!loading && (
@@ -62,6 +63,44 @@ export default function Home() {
             </TabMenu>
           </div>
         )}
+      </section>
+      <section className='container mx-auto p-4'>
+        <Card className='mx-auto w-full max-w-3xl'>
+          <CardBody className='p-5 lg:p-10'>
+            <h2 className='text-xl md:text-3xl font-bold mb-8 text-center'>
+              カードタイプのリンクジェネレーター
+            </h2>
+            <p className='mt-4 leading-relaxed'>
+              「デザイン」✕「テーマカラー」✕「ボーダーの有無」を組み合わせることができます。
+              <br />
+              コードは「code」タブをクリックして、
+              <svg
+                aria-hidden='true'
+                fill='none'
+                focusable='false'
+                height='1em'
+                role='presentation'
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='1.5'
+                viewBox='0 0 24 24'
+                width='1em'
+                className='text-inherit opacity-100 scale-100 inline-block mx-1'
+              >
+                <path d='M16 17.1c0 3.5-1.4 4.9-4.9 4.9H6.9C3.4 22 2 20.6 2 17.1v-4.2C2 9.4 3.4 8 6.9 8h4.2c3.5 0 4.9 1.4 4.9 4.9Z' />
+                <path d='M8 8V6.9C8 3.4 9.4 2 12.9 2h4.2C20.6 2 22 3.4 22 6.9v4.2c0 3.5-1.4 4.9-4.9 4.9H16' />
+                <path d='M16 12.9C16 9.4 14.6 8 11.1 8' />
+              </svg>
+              をクリックしてコピーできます。
+            </p>
+            <p className='mt-4 leading-relaxed'>
+              入力したURLのOGP情報をもとにカードが作成されます。
+              <br />
+              URLによっては、正しくカードが作成されません。ご了承ください。
+            </p>
+          </CardBody>
+        </Card>
       </section>
     </main>
   )
